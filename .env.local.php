153318c12b5db90bba7b287a6fb6c966db439ace
Ryan Coble <ryan.coble@rocketcode.io>
2015-11-11 07:06:57 -0500

    Added config files

---
+++ .env.local.php
@@ -0,0 +1,18 @@
+<?php return array(
+	
+	//MySQL
+	'DB_HOST' => '127.0.0.1',
+	'DB_NAME' => 'remedy',
+	'DB_USER' => 'homestead',
+	'DB_PASS' => 'secret',
+
+	//Shopify
+	'APP_API_KEY'		=> '2b699a7035674b0c3c9c73817cec8bab',
+	'APP_API_SECRET'	=> 'fb34ab862a93803da864166b66a63f2c',
+	'API_ACTK_KEY'      => 'E*8Y^BiXx&SZtdyyNEMocyDt$z8im$$y',
+	//'APP_API_SCOPE'	=> '["read_products", "write_products", "read_script_tags", "write_script_tags", "read_themes", "write_themes"]',
+	'APP_API_SCOPE'		=> '["read_products"]',
+	
+	//Application
+	'HOST' => 'https://remedy.dev',
+);
